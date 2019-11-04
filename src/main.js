e27fd06cff73f8858d46c012183a5058fa0c50a0
youngsoo jung <youngsoo.j@gmail.com>
2019-11-04 15:47:40 +0900

    del comment

--- src/main.js
+++ src/main.js
@@ -7,21 +7,13 @@ import Bar from "@/components/plugin/Bar";
 
 Vue.use(VueRouter);
 
-// 2. Define some routes
-// Each route should map to a component. The "component" can
-// either be an actual component constructor created via
-// `Vue.extend()`, or just a component options object.
-// We'll talk about nested routes later.
 const routes = [
   { path: "/foo", component: Foo },
   { path: "/bar", component: Bar }
 ];
 
-// 3. Create the router instance and pass the `routes` option
-// You can pass in additional options here, but let's
-// keep it simple for now.
 const router = new VueRouter({
-  routes // short for `routes: routes`
+  routes
 });
 
 Vue.config.productionTip = false;
